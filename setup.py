560634f3131651956eae58982b6768a37b6e4d80
Alasdair Nicol <alasdair@thenicols.net>
2015-05-30 14:46:37 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-mustache',
+      version='0.1',
+      author='Alasdair Nicol',
+      author_email='alasdair@thenicols.net',
+      packages=['django_mustache'],
+      requires=['pystache', 'django (>=1.8)'],
+)
